f044db6090e5b70ace37c7819912ba825e1dab7e
ItsA1ive <68302989+ItsA1ive@users.noreply.github.com>
2020-07-14 16:14:49 -0500

    Add files via upload

---
+++ Triangulo 3.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Triangulo_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = 56.61;
+            double z = 3;
+
+            double cRadianes = c * Math.PI / 180;
+
+            double y = z / Math.Tan(cRadianes);
+            double t = Math.Sqrt(y * y + z * z);
+            double a = Math.Atan(y / z);
+
+            double aGrados = a * 180 / Math.PI;
+
+            Console.WriteLine("El lado y es: " + y);
+            Console.WriteLine("El lado t es: " + t);
+            Console.WriteLine("El angulo a es: " + aGrados);
+
+        }
+    }
+}
