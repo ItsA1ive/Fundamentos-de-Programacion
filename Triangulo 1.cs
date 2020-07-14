1724ceb9736618c2a4452e4e60352ed070923f81
ItsA1ive <68302989+ItsA1ive@users.noreply.github.com>
2020-07-14 16:12:53 -0500

    Add files via upload

---
+++ Triangulo 1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double z = 3;
+            double y = 2;
+
+            double a = Math.Atan(y / z);
+            double c = Math.Atan(z / y);
+            double t = Math.Sqrt(y * y + z * z);
+
+            double aGrados = a * 180 / Math.PI;
+            double cGrados = c * 180 / Math.PI;
+
+            Console.WriteLine("Valor de a: = " + aGrados);
+            Console.WriteLine("Valor de c: = " + cGrados);
+            Console.WriteLine("Valor de t: = " + t);
+
+        }
+    }
+}
