4484271de06f351b8a7e51a1d602c08d83e51163
ItsA1ive <68302989+ItsA1ive@users.noreply.github.com>
2020-07-14 16:15:45 -0500

    Add files via upload

---
+++ Triangulo doble 1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Clase_2_ejercicio_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Datos iniciales
+            Console.Write("Ingrese el angulo b: ");
+            double b = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el lado z: ");
+            double z = double.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el lado y: ");
+            double y = double.Parse(Console.ReadLine());
+
+            //Analizando datos
+            double bRad = b * Math.PI / 180;
+
+            double t = Math.Sqrt(z * z + y * y);
+
+            double a = Math.Atan(y / z);
+
+            double d = Math.PI - (bRad + a) - (Math.PI / 2);
+
+            double x = t * Math.Sin(bRad) / Math.Sin(d);
+
+            //Salida de datos
+            Console.WriteLine("El lado x es igual a: " + x);
+
+        }
+    }
+}
